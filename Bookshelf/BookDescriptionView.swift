ccc1e53800e1911c1c79c8ea1f7aaab340f62fc9
Laura Gongaware <LauraGongaware@gmail.com>
2022-07-08 16:01:25 -0700

    completed project, cleaned up appearance and added delete function

---
+++ Bookshelf/BookDescriptionView.swift
@@ -0,0 +1,20 @@
+//
+//  BookDescriptionView.swift
+//  Bookshelf
+//
+//  Created by Laura Gongaware on 7/8/22.
+//
+
+import SwiftUI
+
+struct BookDescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookDescriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookDescriptionView()
+    }
+}
